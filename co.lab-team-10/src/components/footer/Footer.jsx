db06be7b4154d0c49a8ce202ec98ccb7b8dcc303
Zohaib <zohaibmanzoor1700@gmail.com>
2022-09-21 20:52:23 -0700

    Footer Updated for desktop

--- co.lab-team-10/src/components/footer/Footer.jsx
+++ co.lab-team-10/src/components/footer/Footer.jsx
@@ -31,18 +31,31 @@ function Footer() {
           >
             FreeUp
           </Typography>
-          <div className="footer-follow-us">Follow Us</div>
-        </div>
-      </div>
-      <div className="footer-socials">
-        <div className="footer-social-icon1">
-          <ImFacebook2 />
-        </div>
-        <div className="footer-social-icon2">
-          <AiFillInstagram />
-        </div>
-        <div className="footer-social-icon3">
-          <FaTiktok />
+          <div className="about-footer">
+            <div className="about-text">About</div>
+            <div className="extra-text">Company</div>
+            <div className="extra-text">Contact</div>
+            <div className="extra-text">FAQ</div>
+          </div>
+          <div className="community-footer">
+            <div className="community-text">Community</div>
+            <div className="extra-text">News</div>
+            <div className="extra-text">Career</div>
+          </div>
+          <div className="support-footer">
+            <div className="support-text">Support</div>
+            <div className="extra-text">Policies</div>
+            <div className="extra-text">Terms</div>
+            <div className="extra-text">Privacy</div>
+          </div>
+          <div className="follow-us-footer">
+            <div className="follow-us-text">Follow Us</div>
+            <div className="socials-container">
+              <div className="facebook-icon"><ImFacebook2 /></div>
+              <div className="instagram-icon"><AiFillInstagram /></div>
+              <div className="tiktok-icon"><FaTiktok /></div>
+            </div>
+          </div>
         </div>
       </div>
       <div className="copyright-text">©2022 FreeUp, Inc. All rights reserved.</div>
